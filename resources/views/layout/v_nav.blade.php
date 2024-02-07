45b226af41dd6ba55961b91c7c106345ccc2b98f
mramadhanurh <mramadhanur@gmail.com>
2024-02-07 08:41:56 +0700

    update dashboard admin

---
+++ resources/views/layout/v_nav.blade.php
@@ -0,0 +1,19 @@
+<ul id="accordion-menu">
+    <li>
+        <a href="index.html" class="dropdown-toggle no-arrow">
+            <span class="micon bi bi-house"></span><span class="mtext">Dashboard</span>
+        </a>
+    </li>
+
+    <li>
+        <a href="index.html" class="dropdown-toggle no-arrow">
+            <span class="micon bi bi-columns-gap"></span><span class="mtext">Kategori</span>
+        </a>
+    </li>
+
+    <li>
+        <a href="index.html" class="dropdown-toggle no-arrow">
+            <span class="micon bi bi-folder-check"></span><span class="mtext">Produk</span>
+        </a>
+    </li>
+</ul>
